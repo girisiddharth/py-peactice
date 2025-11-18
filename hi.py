53111d55549bd1da21a508e33e7f3f3b48a03828
giri siddharth <siddharth.goswami.7890@gmail.com>
2025-11-18 08:33:16 +0530

    Add files via upload
    code 1

---
+++ hi.py
@@ -0,0 +1,24 @@
+first = int (input("enter first num : "))
+ope = input ("select op")
+second =int (input("enter 2nd"))
+
+
+if (ope == "+"):
+    print(first+second)
+
+elif(ope== "-"):
+    print(first-second)
+
+elif(ope== "/"):
+    print(first/second)
+
+elif(ope== "*"):
+    print(first*second)
+
+elif(ope== "%"):
+    print(first%second)
+
+else: 
+    ("invalid op")
+
+
